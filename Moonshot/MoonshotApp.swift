eee35296e10a2d119ebed695ee61abd35fd5a56a
francisco.gallegos.luque <franciscogallegosluque@gmail.com>
2025-01-28 09:00:03 -0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Francisco Manuel Gallegos Luque on 28/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
